e6b11365e35364be92cbde700940a3a0faba07d8
Alexis <Alexis.donato95@gmail.com>
2020-02-29 14:47:38 -0300

    change variable name

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -111,9 +111,9 @@ app.post('/packs/buy', async (req, res) => {
       const card2Selected = availableCards.splice(random2, 1)[0];
 
       // clean de array from Normal cards for the last selection
-      const cardsBetterThanNormal = availableCards.filter( (card: { rarity: string; }) => card.rarity === 'R' );
-      const random3 = Math.floor(Math.random() * cardsBetterThanNormal.length);
-      const card3Selected = cardsBetterThanNormal[random3];
+      const rareCards = availableCards.filter( (card: { rarity: string; }) => card.rarity === 'R' );
+      const random3 = Math.floor(Math.random() * rareCards.length);
+      const card3Selected = rareCards[random3];
 
       // Buscamos la rareza mas alta de las 3
       const cardsToSend = [card1Selected, card2Selected, card3Selected];
