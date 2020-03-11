fb0794d05326239ce9b64a042085e6fc442a3ea4
Alexis <Alexis.donato95@gmail.com>
2020-03-10 21:42:01 -0300

    bore hizo api de todas las cartas. Deployed

--- src/app/constants/endpoint.ts
+++ src/app/constants/endpoint.ts
@@ -1,6 +1,6 @@
 export const ENDPOINT = {
   URL: 'http://ec2-34-219-173-188.us-west-2.compute.amazonaws.com:8080/buypack',
   URL_CARD: `https://us-central1-ygo-ud.cloudfunctions.net/webApi/api/v1/cards`,
-  URL_ALL_CARDS: `https://us-central1-ygo-ud.cloudfunctions.net/webApi/api/v1/cards`,
+  URL_ALL_CARDS: `http://ec2-34-219-173-188.us-west-2.compute.amazonaws.com:8080/cards`,
   URL_CARD_DETAILS: `https://db.ygoprodeck.com/api/v6/cardinfo.php?id=`
 };
